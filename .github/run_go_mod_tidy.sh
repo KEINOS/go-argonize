246748fb452eab1333631f3709e8840392165e31
KEINOS <github@keinos.com>
2022-08-05 19:41:09 +0900

    feat: weekly go.mod update

--- .github/run_go_mod_tidy.sh
+++ .github/run_go_mod_tidy.sh
@@ -21,6 +21,11 @@ echo '* Create new blank go.mod ...'
 # Copy the first 4 lines of the go.mod.bak file to the new go.mod file.
 < go.mod.bak head -n 4 > go.mod
 
+echo '* Get latest modules ...'
+go get "github.com/pkg/errors"
+go get "github.com/stretchr/testify"
+go get "golang.org/x/crypto"
+
 echo '* Run go tidy ...'
 go mod tidy
 
